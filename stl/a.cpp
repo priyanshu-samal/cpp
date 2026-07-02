e81ab9a56f4e08908f02c026df9a4b8b29bdc3ca
Priyanshu samal <samalpriyanshu966@gmail.com>
2026-07-02 10:59:25 +0530

    3

--- stl/a.cpp
+++ stl/a.cpp
@@ -8,24 +8,14 @@ using namespace std;
 
 
 int main(){
-    vector<int> vec={1,2,3};
-    vector<int> vec2(3,2);
-    vector<int>vec3(vec);
-    for (auto it=vec.begin();it!=vec.end();it++){
-        cout<<*(it)<<endl;
-    }
-
-    stack<int>s;
-    s.push(1);
-    s.push(12);
-    s.push(21);
-    s.push(11);
-
-    cout<<s.top();
-
+   
     map<int,string>m;
     m.insert({1,"priyanshu"});
-    cout<<m.find(1);
+   auto it = m.find(1);
+
+    if (it != m.end()) {
+        cout << it->second << endl;   // prints: priyanshu
+    }
 
     return 0;
 }
