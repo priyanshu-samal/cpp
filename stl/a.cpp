af587e5b871ff945f1e47b47962a5317555a543e
Priyanshu samal <samalpriyanshu966@gmail.com>
2026-07-02 10:45:39 +0530

    1

--- stl/a.cpp
+++ stl/a.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -9,9 +10,16 @@ int main(){
     vector<int> vec={1,2,3};
     vector<int> vec2(3,2);
     vector<int>vec3(vec);
-
-    for(int val:vec3){
-        cout<<val<<"";
+    for (auto it=vec.begin();it!=vec.end();it++){
+        cout<<*(it)<<endl;
     }
+
+    stack<int>s;
+    s.push(1);
+    s.push(12);
+    s.push(21);
+    s.push(11);
+
+    cout<<s.top();
     return 0;
 }
--- stl/vector.cpp
+++ stl/vector.cpp
@@ -5,9 +5,7 @@ using namespace std;
 
 int main(){
     int arr[3]={5,6,7};
-    for(int i:arr[i]){
-        cout<<arr[i];
-    }
+    
     cout<<"Helooo";
     return 0;
 }
